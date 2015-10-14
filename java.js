97988c4887dae0316fd024d5289060a2130ab5a0
Jordan Starr <jordanbstarr@gmail.com>
2015-10-14 15:36:11 -0700

    Update java.js

--- java.js
+++ java.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
        var Age = $('input[name=Age]').val();
        var CKD = $('input[name=CKD]').val();
        var DM = $('input[name=DM]').val();
-       var prob = 100*((e^0.240343)*(e^(-0.052698*Age))*( e^(0.458596*CKD))*( e^(0.236863*DM)) / (1 +(e^0.240343)*(e^(-0.052698*Age))*( e^(0.458596*CKD))*( e^(0.236863*DM)))); 
-       $('#div1').append("<div class='answer'>Probability of TKA Revision = "+prob.toFixed(3)+"%</div>");
+       var prob = 100*((e^0.240343)*(e^(-0.052698*Age))*(e^(0.458596*CKD))*(e^(0.236863*DM)) / (1 + (e^0.240343)*(e^(-0.052698*Age))*(e^(0.458596*CKD))*(e^(0.236863*DM)))); 
+       $('#div1').append("<div class='answer'>Probability of TKA Revision = "+prob.toFixed(1)+"%</div>");
     });
 });
