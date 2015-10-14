c8f270e623a2ca0c7409175a79ca9f37b2193165
Jordan Starr <jordanbstarr@gmail.com>
2015-10-14 15:26:16 -0700

    Create java.js

---
+++ java.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('button[name=Calculate]').on('click', function(){
+       $('.answer').remove();
+       var Age = $('input[name=Age]').val();
+       var CKD = $('input[name=CKD]').val();
+       var DM = $('input[name=DM]').val();
+       var prob = (e^0.240343)*(e^(-0.052698*Age))*( e^(0.458596*CKD))*( e^(0.236863*DM)) / (1 +(e^0.240343)*(e^(-0.052698*Age))*( e^(0.458596*CKD))*( e^(0.236863*DM))); 
+       $('#div1').append("<div class='answer'>Probability of TKA Revision = "+prob.toFixed(3)+'</div>');
+    });
+});
