68efc53d0d50f1a2d509db76172cc84fee1ddfe3
Jordan Starr <jordanbstarr@gmail.com>
2015-10-15 09:55:39 -0700

    Create server.R

---
+++ server.R
@@ -0,0 +1,9 @@
+library(shiny)
+shinyServer(function(input, output) {
+  calcprob <- reactive({
+    as.percent((e^0.240343)*(e^(-0.052698*input$Age))*( e^(0.458596*input$CKD))*( e^(0.236863*input$DM)) / (1 +(e^0.240343)*(e^(-0.052698*input$Age))*( e^(0.458596*input$CKD))*(e^(0.236863*input$DM))))
+  })
+  output$prob <- renderText({ 
+    paste("TKA Revision Probability = ", calcprob())
+  })
+})
